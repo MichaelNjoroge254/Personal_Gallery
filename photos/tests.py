fff7e477a89d66a476de25489928ac8bdc76442a
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-09-05 18:51:38 +0300

    modified photos/tests.py

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .models import Category,Photo
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.favourite= Category(name = 'Favourite')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.favourite,Category))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.favourite.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+
+class photoTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.saved= photo(name = 'Saved')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.saved,Photo))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.saved.save_photo()
+        photos = photo.objects.all()
+        self.assertTrue(len(photos) > 0)
